29c38857d597ecb9f153abd173702b86abbd6a61
cost6885 <132336835+cost6885@users.noreply.github.com>
2025-06-23 15:57:57 +0900

    Update submitRecord.js

--- src/api/submitRecord.js
+++ src/api/submitRecord.js
@@ -1,19 +1,13 @@
-// src/api/submitRecord.js
-
-// Google Apps Script 웹앱 배포 후 url 복사해서 아래에 붙여넣기
-const SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycbzRfrffVsLeUdwTqFx38pfv_sOFJzEaDd-LHDCPwCh3BsdufU0sqT_hI9K5HLtJHOQb8g/exec";
+// src/api/submitRecord.js (프론트)
+const API_URL = "/api/submit"; // EC2면 http://<서버주소>:4000/api/submit
 
 export default async function submitRecord(data) {
   try {
-    const res = await fetch(SCRIPT_URL, {
+    const res = await fetch(API_URL, {
       method: "POST",
-      headers: { "Content-Type": "text/plain;charset=utf-8" },
-      //   headers: { "Content-Type": "application/json" }, // ← 여기!
-      mode: "cors",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
     });
-    // 결과를 json으로 받아서 반환 (Apps Script 응답이 json)
     return await res.json();
   } catch (err) {
     return { status: "error", message: "기록 제출에 실패했어요 😢" };
